15aec23f7da0cad3072acb117e560c502bfd51cc
Nguyen Thanh Tien <nt.tien@alticast.com>
2017-08-29 13:24:08 +0700

    no message

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,9 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('/__/firebase/3.9.0/firebase-app.js');
-importScripts('/__/firebase/3.9.0/firebase-messaging.js');
-importScripts('/__/firebase/init.js');
+importScripts('https://www.gstatic.com/firebasejs/4.3.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.3.0/firebase-messaging.js');
+//importScripts('/__/firebase/init.js');
 
 const messaging = firebase.messaging();
 
